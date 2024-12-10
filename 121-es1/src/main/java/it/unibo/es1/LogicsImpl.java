07b712c944ed52297411b8d948c823f6ea6201bd
MatteoTodeschi <matteo.todeschi2@studio.unibo.it>
2024-12-10 15:48:16 +0100

    Corrected Exercise 121

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,66 +1,50 @@
 package it.unibo.es1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LogicsImpl implements Logics {
-	private final static int MAX_BUTTON_VALUE = 4;
 
-	private final int size;
 	private final List<Integer> buttonList;
-	private final List<Boolean> buttonEnabled;
 
 	public LogicsImpl(int size) {
-		this.size = size;
-		this.buttonList = IntStream
-			.range(0, this.size)
-			.mapToObj(i -> 0)
-			.collect(Collectors.toCollection(ArrayList::new));
-		this.buttonEnabled = IntStream
-			.range(0, this.size)
-			.mapToObj(i -> true)
-			.collect(Collectors.toCollection(ArrayList::new));
+		this.buttonList = new ArrayList<>(Collections.nCopies(size,0));
 	}
 
 	@Override
 	public int size() {
-		return this.size;
+		return buttonList.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		return List.copyOf(this.buttonList);
+		return Collections.unmodifiableList(this.buttonList);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		return List.copyOf(this.buttonEnabled);
+		return buttonList.stream().map(it -> it < size()).toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		this.buttonList.set(elem, this.buttonList.get(elem) + 1);
-		if(this.buttonList.get(elem) == MAX_BUTTON_VALUE){
-			this.buttonEnabled.set(elem,false);
-		}
-		return this.buttonList.get(elem);
+		final int newValue = this.buttonList.get(elem) + 1;
+		this.buttonList.set(elem, newValue);
+		return newValue;
 	}
 
 	@Override
 	public String result() {
-		final StringBuilder result = new StringBuilder();
-		result.append("<<");
-		for (int i = 0; i < this.size; i++) {
-			result.append(this.buttonList.get(i) + (i < this.size-1?"|":""));
-		}
-		result.append(">>");
-		return result.toString();
+		return buttonList.stream()
+			.map(it -> Integer.toString(it))
+			.collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		return this.buttonList.stream().allMatch(i -> i.equals(this.buttonList.get(0)));
+		final var first = this.buttonList.getFirst();
+		return first != 0 && this.buttonList.stream().allMatch(i -> i == first);
 	}
 }
